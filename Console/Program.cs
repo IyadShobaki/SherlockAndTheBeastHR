738b6154ed87afefcc5ca84831562602b825bfaf
IyadShobaki <iyadshobaki82@gmail.com>
2021-01-26 20:54:33 -0500

    Initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+
+    //  Sherlock and The Beast  -- HackerRank
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DecentNumber(1); // -1
+            DecentNumber(3); // 555
+            DecentNumber(5); // 33333
+            DecentNumber(11); // 55555533333
+            DecentNumber(2); // -1
+            DecentNumber(15); // 555555555555555
+            DecentNumber(13); // 5553333333333
+
+            Console.ReadKey();
+        }
+        public static void DecentNumber(int n)
+        {
+            int fivesNumber = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    fivesNumber = i;
+                }
+            }
+            int threesNumber = n - fivesNumber;
+            while (threesNumber % 5 != 0 && fivesNumber > 0)
+            {
+                threesNumber += 3;
+                fivesNumber -= 3;
+            }
+            if (fivesNumber % 3 == 0 && threesNumber % 5 == 0)
+            {
+                Console.WriteLine($"{new string('5', fivesNumber)}{new string('3', threesNumber)}");
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
